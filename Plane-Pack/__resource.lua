f6dc5b69cd31be58992ba41ae96249a2643bebf6
BenSon <contact@cmurphy.dev>
2020-10-08 17:52:51 +0100

    Final Build 1.0
    This FiveM Plane Pack is completed! No need to be improved on as all vehicles have been tested and work fine!

---
+++ Plane-Pack/__resource.lua
@@ -0,0 +1,19 @@
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+
+files {
+	'carvariations.meta',
+	'dlctext.meta',
+	'handling.meta',
+	'vehicles.meta',
+	'carcols.meta',
+	'vehiclelayouts.meta',
+	'carcontentunlocks.meta'    -- Not Required
+}
+
+data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
+data_file 'DLCTEXT_FILE' 'dlctext.meta'
+data_file 'HANDLING_FILE' 'handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
+data_file 'CARCOLS_FILE' 'carcols.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'vehiclelayouts.meta'
+data_file 'CARCONTENTUNLOCKS_FILE' 'carcontentunlocks.meta'   -- Not Required
